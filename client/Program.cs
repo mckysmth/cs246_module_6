e530902e8b729322841dd3c83b173a430e12db5f
mckaysmith <mckaysmith11@gmail.com>
2020-12-11 14:56:39 -0700

    first commit

---
+++ client/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;  
+using System.Net;  
+using System.Net.Sockets;  
+using System.Text;  
+  
+public class SynchronousSocketClient {  
+  
+    public static void StartClient() {  
+        // Data buffer for incoming data.  
+        byte[] bytes = new byte[1024];  
+  
+        // Connect to a remote device.  
+        try {  
+            // Establish the remote endpoint for the socket.  
+            // This example uses port 11000 on the local computer.  
+            IPHostEntry ipHostInfo = Dns.GetHostEntry("localhost");  
+            IPAddress ipAddress = ipHostInfo.AddressList[0];  
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress,11000);  
+  
+            // Create a TCP/IP  socket.  
+            Socket sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp );  
+  
+            // Connect the socket to the remote endpoint. Catch any errors.  
+            try {  
+                sender.Connect(remoteEP);  
+  
+                Console.WriteLine("Socket connected to {0}",  
+                    sender.RemoteEndPoint.ToString());  
+  
+                // Encode the data string into a byte array.  
+                string eq = GetUserInput(); 
+                byte[] msg = Encoding.ASCII.GetBytes(eq);  
+  
+                // Send the data through the socket.  
+                int bytesSent = sender.Send(msg);  
+  
+                // Receive the response from the remote device.  
+                int bytesRec = sender.Receive(bytes);  
+                Console.WriteLine(eq + " = {0}",  
+                    Encoding.ASCII.GetString(bytes,0,bytesRec));  
+  
+                // Release the socket.  
+                sender.Shutdown(SocketShutdown.Both);  
+                sender.Close();  
+  
+            } catch (ArgumentNullException ane) {  
+                Console.WriteLine("ArgumentNullException : {0}",ane.ToString());  
+            } catch (SocketException se) {  
+                Console.WriteLine("SocketException : {0}",se.ToString());  
+            } catch (Exception e) {  
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());  
+            }  
+  
+        } catch (Exception e) {  
+            Console.WriteLine( e.ToString());  
+        }  
+    }  
+  
+    public static int Main(String[] args) {  
+        StartClient();  
+        return 0;  
+    }  
+
+    public static string GetUserInput()
+    {
+        Console.WriteLine("Enter equation (e.g. 2+2): ");
+        return Console.ReadLine();
+    }
+}
